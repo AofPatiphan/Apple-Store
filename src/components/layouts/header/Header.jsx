a0618ba93716ba51d74079ea6a2a4207baa12110
AofPatiphan <patiphan.ma@hotmail.com>
2022-03-25 23:22:44 +0700

    responsive navbar

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -7,66 +7,158 @@ function Header() {
     const { handleSubmitLogout, role } = useContext(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
+    const showSettings = (event) => {
+        event.preventDefault();
+    };
     return (
-        <div className="navDefault ">
-            <div className="row">
-                <div className="col-lg-2 col-md-0 col-sm-0 col-xs-0">
-                    {location.pathname === '/login' ? (
-                        <Link className="ps-3" to={'/'}>
-                            <img
-                                src="https://www.apple.com/ac/globalnav/7/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_apple_image__b5er5ngrzxqq_large.svg"
-                                alt=""
-                                className="logoPic"
-                            />
-                        </Link>
-                    ) : (
-                        <></>
-                    )}
-                </div>
-                <div className="col-lg-8 col-md-12 col-sm-12 col-xs-12 d-flex justify-content-around align-items-center navLink">
-                    {location.pathname === '/login' ? (
-                        <></>
-                    ) : (
-                        <>
-                            <Link to={'/'}>
+        <>
+            <div className="navDefault d-none d-lg-block">
+                <div className="row">
+                    <div className="col-lg-2 col-md-0 col-sm-0 col-xs-0 pt-2 pb-2">
+                        {location.pathname === '/login' ? (
+                            <Link className="ps-3" to={'/'}>
                                 <img
-                                    src="https://www.apple.com/ac/globalnav/7/en_US/images/be15095f-5a20-57d0-ad14-cf4c638e223a/globalnav_apple_image__b5er5ngrzxqq_large.svg"
+                                    src="https://res.cloudinary.com/dbtlgaii3/image/upload/v1648224054/Wow/kisspng-apple-logo-apple-desktop-models-5b4a434798a381.9096071615315935436252_glrwju.png"
                                     alt=""
                                     className="logoPic"
                                 />
                             </Link>
-                            <Link to={'/'}>Store</Link>
-                            <Link to={'/mac'}>Mac</Link>
-                            <Link to={'/'}>iPad</Link>
-                            <Link to={'/'}>iPhone</Link>
-                            <Link to={'/'}>Watch</Link>
-                            <Link to={'/'}>Airpods</Link>
-                            <Link to={'/'}>TV & Home</Link>
-                            <Link to={'/'}>Cart</Link>
-                        </>
-                    )}
-                </div>
-                <div className="col-lg-2 col-md-0 col-sm-0 col-xs-0 d-flex justify-content-end align-items-center">
-                    {role === 'guest' ? (
-                        location.pathname !== '/login' && (
+                        ) : (
+                            <></>
+                        )}
+                    </div>
+                    <div className="col-lg-8 col-md-12 col-sm-12 col-xs-12 d-flex justify-content-around align-items-center navLink">
+                        {location.pathname === '/login' ? (
+                            <></>
+                        ) : (
+                            <>
+                                <Link to={'/'}>
+                                    <img
+                                        src="https://res.cloudinary.com/dbtlgaii3/image/upload/v1648224054/Wow/kisspng-apple-logo-apple-desktop-models-5b4a434798a381.9096071615315935436252_glrwju.png"
+                                        alt=""
+                                        className="logoPic"
+                                    />
+                                </Link>
+                                <Link to={'/store'}>Store</Link>
+                                <Link to={'/mac'}>Mac</Link>
+                                <Link to={'/ipad'}>iPad</Link>
+                                <Link to={'/iphone'}>iPhone</Link>
+                                <Link to={'/watch'}>Watch</Link>
+                                <Link to={'/airpods'}>Airpods</Link>
+                                <Link to={'/tv'}>TV & Home</Link>
+                                <Link to={'/cart'}>Cart</Link>
+                            </>
+                        )}
+                    </div>
+                    <div className="col-lg-2 col-md-0 col-sm-0 col-xs-0 d-flex justify-content-end align-items-center">
+                        {role === 'guest' ? (
+                            location.pathname !== '/login' && (
+                                <button
+                                    className="btn navButton"
+                                    onClick={() => navigate('/login')}
+                                >
+                                    Login
+                                </button>
+                            )
+                        ) : (
                             <button
-                                className="btn navButton"
-                                onClick={() => navigate('/login')}
+                                className="btn navButton ms-3 me-4"
+                                onClick={handleSubmitLogout}
                             >
-                                Login
+                                Logout
                             </button>
-                        )
-                    ) : (
-                        <button
-                            className="btn navButton ms-3 me-4"
-                            onClick={handleSubmitLogout}
-                        >
-                            Logout
-                        </button>
-                    )}
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark d-block d-lg-none">
+                <div className="container-fluid">
+                    <Link to={'/'}>
+                        <img
+                            src="https://res.cloudinary.com/dbtlgaii3/image/upload/v1648224054/Wow/kisspng-apple-logo-apple-desktop-models-5b4a434798a381.9096071615315935436252_glrwju.png"
+                            alt=""
+                            className="logoPic"
+                        />
+                    </Link>
+                    {location.pathname !== '/login' ? (
+                        <button
+                            className="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarText"
+                            aria-controls="navbarText"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    ) : (
+                        <></>
+                    )}
+                    <div className="collapse navbar-collapse" id="navbarText">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link active" to={'/store'}>
+                                    Store
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to={'/mac'}>
+                                    Mac
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to={'/ipad'} className="nav-link">
+                                    iPad
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to={'/iphone'} className="nav-link">
+                                    iPhone
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to={'/watch'} className="nav-link">
+                                    Watch
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to={'/airpods'} className="nav-link">
+                                    Airpods
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to={'/tv'} className="nav-link">
+                                    TV & Home
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to={'/cart'} className="nav-link">
+                                    Cart
+                                </Link>
+                            </li>
+                        </ul>
+                        {role === 'guest' ? (
+                            location.pathname !== '/login' && (
+                                <button
+                                    className="btn navButton"
+                                    onClick={() => navigate('/login')}
+                                >
+                                    Login
+                                </button>
+                            )
+                        ) : (
+                            <button
+                                className="btn navButton ms-3 me-4"
+                                onClick={handleSubmitLogout}
+                            >
+                                Logout
+                            </button>
+                        )}
+                    </div>
+                </div>
+            </nav>
+        </>
     );
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import ProductCard from '../../components/products/productcard/ProductCard';
 import './home.css';
 
 function Home() {
     return (
         <div className="homeBody">
             <div>
-                <div className="row">
-                    <div className="col-sm-12 col-xs-12 homeBanner d-flex flex-column justify-content-center">
-                        Banner
-                    </div>
-                    <div className="col-sm-4 col-xs-12 productCardContainer">
-                        <ProductCard />
-                    </div>
-                    <div className="col-sm-4 col-xs-12 productCardContainer">
-                        <ProductCard />
-                    </div>
-                    <div className="col-sm-4 col-xs-12 productCardContainer">
-                        <ProductCard />
-                    </div>
+                <div className="homeBanner">
+                    <div className="headline">iPhone 13 Pro</div>
                 </div>
             </div>
         </div>
